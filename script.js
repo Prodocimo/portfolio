f9ff70eb327a077325a7e23e59e4d1ca0c099aab
João Victor Limeira <jvlprodocimo@gmail.com>
2024-08-11 00:21:30 -0300

    a

---
+++ script.js
@@ -0,0 +1,9 @@
+const Projects = document.getElementById("card-projects")
+
+function ClickProjects() {
+    if (Projects.style.display == 'block') {
+        Projects.style.display = 'none'
+    } else {
+        Projects.style.display = 'block'
+    }
+}
